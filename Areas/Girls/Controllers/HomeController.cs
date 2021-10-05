d7861e91c8e8935177086614407d0859c9e4a48e
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-10-04 20:26:58 -0400

    Added in new comments for the final submission of the assignment.

--- Areas/Girls/Controllers/HomeController.cs
+++ Areas/Girls/Controllers/HomeController.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace Areas.Areas.Girls.Controllers
 {
+    /// <summary>
+    /// This is the home controller for the Girls area.
+    /// Because of the routes specified by the startup file, the app will know to go to the index method of this controller when called upon.
+    /// </summary>
     [Area("Girls")]
     public class HomeController : Controller
     {
         //[Route("[area]/[controller]")]
+        //Create a greeting property for the ViewBag unique to the boys area o use in the index file
         public IActionResult Index()
         {
             ViewBag.Greeting = "Girls just want to have fun";
--- Areas/Teachers/Controllers/HomeController.cs
+++ Areas/Teachers/Controllers/HomeController.cs
@@ -6,9 +6,14 @@ using System.Threading.Tasks;
 
 namespace Areas.Areas.Teachers.Controllers
 {
+    /// <summary>
+    /// This is the home controller for the Teachers area.
+    /// Because of the routes specified by the startup file, the app will know to go to the index method of this controller when called upon.
+    /// </summary>
     [Area("Teachers")]
     public class HomeController : Controller
     {
+        //Create a greeting property for the ViewBag unique to the boys area o use in the index file
         public IActionResult Index()
         {
             ViewBag.Greeting = "Hello Teachers!";
